3ced893d649f8116b7a2715a23424e50ce0a691b
Oskar Korczak <oskar.korczak@protonmail.com>
2022-12-05 10:41:41 +0100

    part 2 done

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -1,5 +1,9 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
 fn main() {
-    let missiles = 8;
-    let ready = 2;
-    println!("firing {} of my {} missiles", ready, missiles)
+    let mut missiles = STARTING_MISSILES;
+    let ready = READY_AMOUNT;
+    println!("firing {} of my {} missiles", ready, missiles);
+    missiles = missiles - ready;
+    println!("{} missiles left", missiles);
 }
